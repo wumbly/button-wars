d1ef4a17090fba8f93a5d6c93b8178d70f816d69
Daniel Lewis <helveticascenario@Daniels-MacBook-Pro.local>
2017-12-10 00:59:12 -0600

    player connection and action messages

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
+const Players = require('./server/players');
+console.log(Players);
 
 io.on('connection', function(socket) {
+	const { id: playerId } = Players.makePlayer();
+	socket.on('disconnect', () => {
+		Players.deletePlayer(playerId);
+		io.emit('playerDisconnected', playerId);
+	});
+	socket.on('newPosition', e => {
+		console.log(e);
+	});
+	socket.on('clicked', e => {
+		console.log(e);
+	});
+	io.emit('playerConnected', playerId);
 	socket.on('chat message', function(msg) {
 		console.log('message: ' + msg);
 		io.emit('send message', 'got message back');
--- server/players.js
+++ server/players.js
@@ -2,31 +2,38 @@ const uuid = require('uuid/v4');
 
 const players = new Map();
 
-export const makePlayer = () => {
-	const player = {
+const makePlayer = () => {
+	const player = Object.freeze({
 		id: uuid(),
 		wins: 0,
 		losses: 0,
 		games: 0,
-	};
+	});
 	players.set(player.id, player);
 	return player;
 };
 
-export const getPlayer = id => ({ ...players.get(id) });
+const getPlayer = id => players.get(id);
 
-export const incrementPlayerStats = (id, winInc, lossInc, gamesInc) => {
+const incrementPlayerStats = (id, winInc, lossInc, gamesInc) => {
 	const player = players.get(id);
-	const newPlayer = {
+	const newPlayer = Object.freeze({
 		...player,
 		wins: player.wins + winInc,
 		losses: player.losses + lossInc,
 		games: player.games + gamesInc,
-	};
+	});
 	players.set(id, newPlayer);
 	return newPlayer;
 };
 
-export const deletePlayer = id => {
+const deletePlayer = id => {
 	player.delete(id);
+};
+
+module.exports = {
+	makePlayer,
+	getPlayer,
+	deletePlayer,
+	incrementPlayerStats,
 };
